b94f73b0522bc23b94db5526ccd8f63660623760
Frances Saunders <92679675+frances-saunders@users.noreply.github.com>
2022-09-14 22:19:12 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,3 +1,7 @@
+/*This project fulfills the reqs for "Business Outfitted Bob" project in Codecademy
+This file is to be used in conjuncition with StartScene.js, GameScene.js, EndScene.js, and index.html*/
+
+
 const config = {
   type: Phaser.AUTO,
   width: 480,
